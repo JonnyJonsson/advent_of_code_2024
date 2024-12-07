fdf2fb0cd3c269143e24cd6e6bd4b4503a271711
unknown <amoniaken@hotmail.com>
2024-12-07 16:05:46 +0100

    day 3 start

--- day3.py
+++ day3.py
@@ -7,15 +7,37 @@ with open("day3_input", "r") as f:
 	reader = csv.reader(f, delimiter=" ")
 	for row in reader:
 		input.append(row)
-sum = 0
-# mul(xxx,yyy)
+multiplication = 0
+
+
+def get_sum_of_mul(s):
+	result = re.findall(r"mul\(\d{1,3},\d{1,3}\)", s)
+	sum = 0
+	if result:
+		for mul in result:
+			# print(mul)
+			x = re.search(r"\(\d{1,3}", mul)
+			y = re.search(r",\d{1,3}", mul)
+			sum += int(x.group()[1:]) * int(y.group()[1:])
+			# print(f"sum: {sum}")
+	return sum
+
+
 for row in input:
 	for s in row:
-		result = re.findall(r"mul\(\d{1,3},\d{1,3}\)", s)
-		if result is not None:
-			for mul in result:
-				# print(mul)
-				x = re.search(r"\(\d{1,3}", mul)
-				y = re.search(r",\d{1,3}", mul)
-				sum = +int(x.group()[1:]) * int(y.group()[1:])
-print(sum)
+		multiplication += get_sum_of_mul(s)
+
+print(multiplication)
+
+
+def find_do_segments(s):
+	result = re.findall(r"do\(\).*don't\(\)", s)
+	if result:
+		for seg in result:
+			print(seg)
+			print("\n")
+
+
+for row in input:
+	for s in row:
+		find_do_segments(s)
