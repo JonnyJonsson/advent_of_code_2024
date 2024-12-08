2092b03140664e2eea1773788cb47081f413732c
unknown <amoniaken@hotmail.com>
2024-12-08 17:00:49 +0100

    day4 part1

--- day4.py
+++ day4.py
@@ -3,6 +3,8 @@ import numpy as np
 
 input = []
 XMAS = ["X", "M", "A", "S"]
+# test = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+xmas_sum = 0
 
 with open("day4_input", "r") as f:
 	reader = csv.reader(f, delimiter=" ")
@@ -15,24 +17,43 @@ for row in input:
 	for c in row:
 		for c2 in c:
 			row_l.append(c2)
-	# print(row_l)
-	# print("next")
 	matrix = np.append(matrix, [row_l], axis=0)
-diags = [matrix[::-1, :].diagonal(i) for i in range(-139, 140)]
-diags.extend(matrix.diagonal(i) for i in range(139, -140, -1))
-# print([n.tolist() for n in diags])
 
-xmas_sum = 0
 
-for dia in diags:
-	diagonal = dia.tolist()
+def calc_xmas(line=["X", "X", "M", "A", "S", "X", "M", "A", "S"]):
+	occurence = 0
 	indexes = []
-	for i in range(len(diagonal)):
-		if diagonal[i : i + len(XMAS)] == XMAS:
+	for i in range(len(line)):
+		if line[i : i + len(XMAS)] == XMAS:
 			indexes.append((i, i + len(XMAS)))
-			xmas_sum += 1
-	for i in indexes:
-		for x in range(i[0], i[1]):
-			print(diagonal[x])
+			occurence += 1
+
+	return occurence
+
+
+for i in range(0, 4):
+	matrix = np.rot90(matrix, k=i)
+	diags = [matrix[::1, :].diagonal(i) for i in range(-matrix.shape[0] + 1, matrix.shape[1])]
+
+	for dia in diags:
+		# print(dia.tolist())
+		xmas_sum += calc_xmas(dia.tolist())
+print(f"xmas_sum after diagonals: {xmas_sum}")
+
+for line in matrix:
+	xmas_sum += calc_xmas(line.tolist())
+print(f"xmas_sum after left-right {xmas_sum}")
+
+for line in np.fliplr(matrix):
+	xmas_sum += calc_xmas(line.tolist())
+print(f"xmas_sum after right-left {xmas_sum}")
+
+for line in matrix.T:
+	xmas_sum += calc_xmas(line.tolist())
+print(f"xmas_sum after top-down {xmas_sum}")
+
+for line in np.flipud(matrix).T:
+	xmas_sum += calc_xmas(line.tolist())
+print(f"xmas_sum after down-top {xmas_sum}")
 
 print(f"xmas sum:  {xmas_sum}")
