d907302b7e4175d288564df24424fd4edf37e691
unknown <amoniaken@hotmail.com>
2024-12-12 21:40:27 +0100

    da54

--- day5.py
+++ day5.py
@@ -10,7 +10,8 @@ with open("day5_input", "r") as f:
 	reader = csv.reader(f, delimiter=" ")
 	for row in reader:
 		tree.append(row)
-print_queue = ["75", "97", "47", "61", "53"]
+print_queue = ["75", "47", "61", "53", "29"]
+print(f"print_queue: {print_queue}")
 graph = {
 	"47": {"53", "13", "61", "29"},
 	"97": {"13", "61", "47", "29", "53", "75"},
@@ -22,30 +23,33 @@ graph = {
 ts = TopologicalSorter(graph)
 order = ts.static_order()
 nodes = list(order)
-# print(nodes)
 printed = []
+print(f"nodes order: {nodes}")
+middle_page_num = 0
 
 for paper in print_queue:
 	# All nodes that need to be before paper
 	temp = [n for n in nodes if nodes.index(n) > nodes.index(paper)]
-	# print(f"paper: {paper}")
-	print(temp)
-	# print(printed)
+	print(f"temp: {temp}")
 	for p in printed:
 		if p in temp:
 			temp.remove(p)
-			# print(f"removed {p} from temp: {temp}")
+
+	for p in nodes:
+		if p not in print_queue:
+			if p in temp:
+				temp.remove(p)
+
 	# See if print_queue is in the right order
-	print("y")
-	print(temp)
-	print([nodes.index(n) for n in nodes if n in temp])
-	if (
-		nodes.index(paper) > all([nodes.index(n) for n in nodes if n in temp])
-		or print_queue.index(paper) == len(print_queue) - 1
-	):
+	if print_queue.index(paper) == len(print_queue) - 1:
 		print(f"paper {paper} is in right order")
+		middle_page_num += print_queue[len(print_queue) // 2]
+	elif all(nodes.index(paper) > nodes.index(n) for n in temp):
+		print(f"paper: {paper} in rigt order")
 	else:
-		print("wrong order")
+		print(f"paper: {paper} in wrong order")
 		break
 
 	printed.append(paper)
+
+print(f"sum of middle page nums: {middle_page_num} ")
